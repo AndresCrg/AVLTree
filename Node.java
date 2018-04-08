eb72a475c4a478f19f80aa1cb73aa88b4b04b95f
AndresCrg <brayan.cardenas@uptc.edu.co>
2018-04-08 13:57:17 -0500

    Clase Nodo con sus metodos correspondientes

--- Node.java
+++ Node.java
@@ -1,5 +1,41 @@
 package models;
 
 public class Node {
+	
+	private int info;
+	private Node left;
+	private Node right;
+	
+	public Node(int info) {
+		this.info = info;
+	}
 
+	public int getInfo() {
+		return info;
+	}
+
+	public void setInfo(int info) {
+		this.info = info;
+	}
+
+	public Node getLeft() {
+		return left;
+	}
+
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+
+	public Node getRight() {
+		return right;
+	}
+
+	public void setRight(Node right) {
+		this.right = right;
+	}
+
+	@Override
+	public String toString() {
+		return "Node [info=" + info + "]";
+	}
 }
